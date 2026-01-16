78e0c493dd918de727c372b0ce429ed66e23db2e
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-01-17 02:09:37 +0600

    basic types

--- script.ts
+++ script.ts
@@ -1,5 +1,56 @@
-console.log("hello");
+// console.log("hello");
 
-const country="I loveBangladesh";
+// const country="I love Bangladesh";
 
-console.log(country);
+// console.log(country);
+
+// let playerName="Mashrafi";
+// let age=34;
+
+// console.log(playerName);
+
+// age="sakib";
+
+// console.log(playerName)
+
+// ------------
+
+// let playerName;
+
+// playerName="sakib";
+
+// playerName=34;
+
+// console.log(playerName)
+
+// -------------
+
+// function multiply(a: number,b: number){
+//     return a*b;
+// }
+
+// console.log(multiply(8,6));
+
+// --------
+
+//array
+
+// let fruits=["apple", "banana", "mango"];
+
+// fruits.push(23);
+
+// let mixed=["apple", 23, true];
+
+// mixed.push(23);
+
+// --------
+
+// object
+
+// let person={
+//     name: "mashrafi",
+//     age:23,
+//     isCaptain:true,
+// };
+
+// person.name="string"
