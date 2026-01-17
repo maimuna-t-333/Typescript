3a510793c9c8d3855b386903ec8013bc5bde4f20
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-01-17 19:41:42 +0600

    Explicit and union types

--- output/script.js
+++ output/script.js
@@ -31,4 +31,27 @@ export {};
 //     isCaptain:true,
 // };
 // person.name="string"
+// ------------
+// let a: string;
+// let b: number;
+// a = "panda";
+// b = 32;
+//array
+// let c: (string | number)[] = [];
+// c.push(7)
+// c.push("sumit")
+//object
+// let c:{
+//     name:string,
+//     age:number,
+//     adult:boolean
+// }
+// c={
+//     name:"panda",
+//     age:20,
+//     adult:true
+// }
+// let c:object;
+// c=[7,43,64,21];
+// NB: array is a type of object
 //# sourceMappingURL=script.js.map
