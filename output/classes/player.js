a3630a4a3f74ac2c1d2d84963a0b790f38b180b4
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-01-19 00:45:14 +0600

    module systems

---
+++ output/classes/player.js
@@ -0,0 +1,11 @@
+export class player {
+    constructor(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+    play() {
+        console.log(`${this.name} from ${this.country} is playing`);
+    }
+}
+//# sourceMappingURL=player.js.map
---
+++ output/src/classes/player.d.ts
@@ -0,0 +1,8 @@
+export declare class player {
+    name: string;
+    private age;
+    readonly country: string;
+    constructor(name: string, age: number, country: string);
+    play(): void;
+}
+//# sourceMappingURL=player.d.ts.map
